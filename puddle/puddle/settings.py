dd6dbc68bb7346466e90fd086c08abd31f42ce89
Stevenlub <lubbesteven27@gmail.com>
2023-10-14 18:57:18 +0200

    Updated bug errors

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,6 +61,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
             ],
         },
     },
@@ -106,9 +108,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/' 
-MEDIA_ROOT = BASE_DIR / '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics/'),)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -2,6 +2,7 @@ import sys
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
